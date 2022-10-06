92459a0c8f8e733ba337979d37ccd80009b68437
cabanaslamarquesa <105670670+cabanaslamarquesa@users.noreply.github.com>
2022-10-06 17:33:06 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Menu Mobile
+var menu = document.getElementById("button_menu_mobile")
+var menu_open = document.getElementById("mobile_menu_option")
+menu.addEventListener("click", ()=>{
+    menu_open.classList.toggle("menu_option-open")
+})
