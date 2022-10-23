05205c871abc8100517737f2f629fe9a127d5e9d
Thevutman <cabanaslamarquesa@gmail.com>
2022-10-23 12:54:58 -0500

    java script

--- js/main.js
+++ js/main.js
@@ -4,3 +4,5 @@ var menu_open = document.getElementById("mobile_menu_option")
 menu.addEventListener("click", ()=>{
     menu_open.classList.toggle("menu_option-open")
 })
+
+window.location.href = "../html/maintenance.html";
